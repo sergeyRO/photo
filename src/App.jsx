8fe97ea15649a9ff72938c1e6f13aa165f7466da
SergeyRO <sergey_r.o@mail.ru>
2023-10-29 09:34:22 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DownLoaderFiles from './components/DownLoaderFles'
+
+function App() {
+
+  return (
+    <>
+      <DownLoaderFiles />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/DownLoaderFles.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useState, useRef } from 'react'
+import canceled from '../assets/canceled.svg'
+
+
+
+export default function DownLoaderFiles() {
+    /*const fileRef = useRef();
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        console.log(fileRef.current.files)
+        console.dir(fileRef.current.files)
+    }*/
+
+    const [fileUrls, setUrls] = useState([])
+  
+    const fileToDataUrl = file => {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+      
+        fileReader.addEventListener('load', evt => {
+          //console.log(evt)
+          resolve(evt.currentTarget.result);
+        });
+        
+        fileReader.addEventListener('error', evt => {
+          reject(new Error(evt.currentTarget.error));
+        });
+        
+        fileReader.readAsDataURL(file);
+      });
+    }
+    
+    const handleSelect = async (evt) => {
+        const files = [...evt.target.files];
+        const urls = await Promise.all(files.map(o => fileToDataUrl(o)));
+        // У вас в массиве - dataUrl, можете использовать в качестве значения атрибута src тега img
+        urls.map(url => fileUrls.push(url))
+        setUrls([...fileUrls])
+      }
+    const handleRemove = (index) => {
+      fileUrls.splice(index, 1)
+      setUrls([...fileUrls])
+    }
+
+
+  return (
+    <form>
+        <label htmlFor='file'> 
+        <div style={{
+          pointerEvents: 'none', 
+          border:'1px solid black', 
+          borderRadius:'5px', 
+          width: '600px', 
+          height: '75px'
+          }}>
+          <br/>
+          Click to select
+        </div>
+        </label>
+            <input type='file' id='file' accept="image/*" style={{display: "none"}} multiple onChange={handleSelect}/>   
+            {
+                fileUrls.map((url,index) => 
+                <>
+                  <div key={index} style={{ position:"relative", marginLeft:"30px", marginTop:"20px", float:"left" }}>
+                    <img src={canceled} style={{position:"absolute", margin:"-15px 0 0 120px"}} onClick={() => {handleRemove(index)}}/>
+                    <img src={url} style={{width:"150px",height:"150px"}} />
+                  </div>
+                </>
+                )
+            }
+            
+    </form>
+  )
+}
